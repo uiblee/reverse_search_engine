edd59f375010f8e032007b6578f9bb8a00287696
Moon Limb <moonlimb@gmail.com>
2012-11-08 17:09:43 -0800

    "thursday"

---
+++ reading _text.py
@@ -0,0 +1,36 @@
+from sys import argv
+
+def main():
+
+    filename = argv[1]
+    english = argv[2]
+    f = open(filename)
+    d = open(english)
+    text = f.read()
+    eng = d.read()
+    listed_sentence = text.split(/n)
+    common_word = eng.split()
+    f.close()
+    d.close()
+    
+    for line in listed_sentence:   
+        c = check_dictionary(line, common_word)
+        rest_program(c)
+
+def check_dictionary_line(line, common_words):
+    word_list = line.split()
+    for each_word in word_list:
+        if each_word not in common_words:
+            return word_list
+
+word_list = [] 
+
+def rest_program(word_list)
+
+
+
+
+
+
+
+main()
